f06f601fd6a8bcfa2a0af2505cc22daaae66ce57
inti0236 <inti0236@192.168.1.171>
2017-11-08 18:13:00 +0100

    Benjamin : Début AdminDaoImpl et refait config de la base de données.

--- EcommerceSpring/src/main/java/fr/adaming/dao/AdminDaoImpl.java
+++ EcommerceSpring/src/main/java/fr/adaming/dao/AdminDaoImpl.java
@@ -1,12 +1,30 @@
 package fr.adaming.dao;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import fr.adaming.modele.Administrateur;
 
 public class AdminDaoImpl implements IAdminDao{
 
+	@Autowired
+	private SessionFactory sessionFactory;
+	
 	@Override
 	public Administrateur connexionAdmin(Administrateur administrateur) {
-
+		//Instanciation de la session
+		Session session = sessionFactory.getCurrentSession();
+		//Requête HQL
+		String req ="FROM Administrateur admin WHERE admin.identifiant=:pIdentifiant AND admin.mdp=p:MdP";
+		Query query = session.createQuery(req);
+		//Passage des paramètres.
+		query.setParameter("pIdentifiant", administrateur.getIdentifiant());
+		query.setParameter("p:MdP", administrateur.getMdp());
+		//Exécution de la requête
+		Administrateur administrateurRecup = (Administrateur) query.uniqueResult();
+		
 		return null;
 	}
 
